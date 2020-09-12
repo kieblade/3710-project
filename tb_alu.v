2d39adf3b4b81b343e5220a4562ba826e61deb02
Scott Crowley <scott.crowley@utah.edu>
2020-09-11 18:02:49 -0600

    Update tb_alu.v
    Need to add tests for shifts and wait.
    Then just need to test specific corner cases.

--- tb_alu.v
+++ tb_alu.v
@@ -127,6 +127,121 @@ module tb_alu();
 		end
 		
 		
+		//Random SUB Simulation
+		Opcode = 8'b00001001; #50
+		$display("\n ********* SUB *********   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d - %d = %d   %b", A, B, C, Flags[4:0]);
+		end
+		
+		
+		//Random SUBI Simulation
+		Opcode = 8'b10010000; #50
+		$display("\n ********* SUBI ********   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d - %d = %d   %b", A, B, C, Flags[4:0]);
+		end
+		
+		
+		//Random CMP Simulation
+		Opcode = 8'b00001011; #50
+		$display("\n ********* CMP *********   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d < %d            %b", A, B, Flags[4:0]);
+		end
+		
+		
+		//Random CMPI Simulation
+		Opcode = 8'b10110000; #50
+		$display("\n ********* CMPI ********   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d < %d            %b", A, B, Flags[4:0]);
+		end
+		
+		
+		//Random CMPU Simulation
+		Opcode = 8'b00001000; #50
+		$display("\n ********* CMPU ********   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d < %d              %b", $unsigned(A), $unsigned(B), Flags[4:0]);
+		end
+		
+		
+		//Random CMPUI Simulation
+		Opcode = 8'b00001100; #50
+		$display("\n ********* CMPUI *******   FLAGS:");
+		$display("                           ZCOLN");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("%d < %d              %b", $unsigned(A), $unsigned(B), Flags[4:0]);
+		end
+		
+		
+		//Random AND Simulation
+		Opcode = 8'b00000001; #50
+		$display("\n ********* AND *********");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("  %b", A);
+			$display("* %b", B);
+			$display("------------------");
+			$display("  %b\n", C);
+		end
+		
+		
+		//Random OR Simulation
+		Opcode = 8'b00000010; #50
+		$display("\n ********* OR  *********");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("  %b", A);
+			$display("+ %b", B);
+			$display("------------------");
+			$display("  %b\n", C);
+		end
+		
+		
+		//Random XOR Simulation
+		Opcode = 8'b00000011; #50
+		$display("\n ********* XOR *********");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("  %b", A);
+			$display("^ %b", B);
+			$display("------------------");
+			$display("  %b\n", C);
+		end
+		
+		
+		//Random NOT Simulation
+		Opcode = 8'b00001111; #50
+		$display("\n ********* NOT *********");
+		for( i = 0; i < 10; i = i + 1) begin
+			A = $random % 65536;
+			B = $random % 65536; #50
+			$display("! %b", A);
+			$display("= %b\n", C);
+		end
+		
 		// Cont...
 
 	end
