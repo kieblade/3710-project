2aedfb2c628bc9e6cd05b0a70f258b1492d66ad9
Scott Crowley <scott.crowley@utah.edu>
2020-09-29 17:40:29 -0600

    Update dpram.v
    1st "working" version of ram module. Basically identical to the Quartus template. Need to add initialization next.

--- dpram.v
+++ dpram.v
@@ -1,11 +1,37 @@
 // REPRESENTS A TRUE DUAL-PORT BLOCK RAM WITH 16-BIT WORDS, 512-WORD BLOCKS, & 2 BLOCKS (2KB OF MEMORY)
 
 module dpram (
-	input rst, clk, en_A,   en_B,
+	input clk, en_A, en_B,
 	input  [9:0] addr_A, addr_B,
 	input [15:0] data_A, data_B,
 	output reg [15:0] out_A, out_B
 );
-
-
+	
+	
+	// RAM variable is a 2D array of 1024 16-bit words.
+	reg [15:0] ram [1023:0];
+	
+	// Port A
+	always @(posedge clk) begin
+		if (en_A) begin
+			ram[addr_A] <= data_A;
+			out_A <= data_A;
+		end
+		else begin
+			out_A <= ram[addr_A];
+		end
+	end
+	
+	// Port B
+	always @(posedge clk) begin
+		if (en_B) begin
+			ram[addr_B] <= data_B;
+			out_B <= data_B;
+		end
+		else begin
+			out_B <= ram[addr_B];
+		end
+	end
+	
+	
 endmodule
