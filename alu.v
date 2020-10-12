c882d990c0c30cb44545c71841d6f4c728a279bc
Scott Crowley <scott.crowley@utah.edu>
2020-10-12 10:21:09 -0600

    Update alu.v
    Fixed a couple opcode conflicts for immediate operations.

--- alu.v
+++ alu.v
@@ -30,43 +30,42 @@ output reg [4:0] Flags;
 //Flags[2] flag bit (overflow)
 //Flags[3] carry bit
 //Flags[4] z bit
-parameter ADD = 8'b00000101;
-parameter ADDI = 8'b01010000;
-parameter ADDU = 8'b00000110;
+parameter ADD   = 8'b00000101;
+parameter ADDI  = 8'b01010000;
+parameter ADDU  = 8'b00000110;
 parameter ADDUI = 8'b01100000;
-parameter ADDC = 8'b00000111;
+parameter ADDC  = 8'b00000111;
 parameter ADDCI = 8'b01110000;
 parameter ADDCU = 8'b00000100;
-parameter ADDCUI = 8'b01000000;
-parameter SUB = 8'b00001001;
-parameter SUBI = 8'b10010000;
-parameter CMP = 8'b00001011;
-parameter CMPI = 8'b10110000;
-parameter CMPU = 8'b00001000; 
-parameter CMPUI = 8'b00001100;
+parameter ADDCUI= 8'b10100000;
+parameter SUB   = 8'b00001001;
+parameter SUBI  = 8'b10010000;
+parameter CMP   = 8'b00001011;
+parameter CMPI  = 8'b10110000;
+parameter CMPU  = 8'b00001000; 
+parameter CMPUI = 8'b11000000;
 
-parameter AND = 8'b00000001;
-parameter ANDI = 8'b00010000;
+parameter AND   = 8'b00000001;
+parameter ANDI  = 8'b00010000;
 
-parameter OR = 8'b00000010;
-parameter ORI = 8'b00100000;
-parameter XOR = 8'b00000011;
-parameter XORI = 8'b00110000;
-parameter NOT = 8'b00001111;
-//parameter NOT = 4'b; //to do
+parameter OR    = 8'b00000010;
+parameter ORI   = 8'b00100000;
+parameter XOR   = 8'b00000011;
+parameter XORI  = 8'b00110000;
+parameter NOT   = 8'b00001111;
 
 // shifts
-parameter LSH = 8'b10000100;
-parameter LSHI = 8'b10000000;
-parameter RSH = 8'b10000101;
-parameter RSHI = 8'b10000001;
+parameter LSH   = 8'b10000100;
+parameter LSHI  = 8'b10000000;
+parameter RSH   = 8'b10000101;
+parameter RSHI  = 8'b10000001;
 
-parameter ALSH = 8'b10000110;
+parameter ALSH  = 8'b10000110;
 parameter ALSHI = 8'b10000010;
-parameter ARSH = 8'b10000111;
+parameter ARSH  = 8'b10000111;
 parameter ARSHI = 8'b10000011;
 
-parameter NOP = 8'b00000000;
+parameter NOP   = 8'b00000000;
 
 always @(A, B, carryIn, Opcode)
 begin
